19247df94c195d312aa280a93ab6623e0c6779e5
Matteo Contri <matteo.contri@tecem.it>
2014-06-20 15:33:15 +0200

    add options to preserve files on destination directory

--- Services/FileManager.php
+++ Services/FileManager.php
@@ -109,7 +109,14 @@ class FileManager
                 throw new \Exception("to_folder does not exist");
             }
             $result = null;
-            system("rsync -a --delete " . escapeshellarg($from . '/') . " " . escapeshellarg($to), $result);
+            if (isset($options['preserve_to_files']) && $options['preserve_to_files'])
+            {
+                system("rsync -a" . escapeshellarg($from . '/') . " " . escapeshellarg($to), $result);
+            }
+            else
+            {
+                system("rsync -a --delete " . escapeshellarg($from . '/') . " " . escapeshellarg($to), $result);
+            }
             if ($result !== 0)
             {
                 throw new \Exception("Sync failed");
